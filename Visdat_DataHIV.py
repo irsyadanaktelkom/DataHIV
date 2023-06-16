b137a75aaa842bd6bff6c8f8480c3e3b9e83fba2
irsyadfnurdin23 <irsyadfauzan28@gmail.com>
2023-06-16 17:18:39 +0700

    Update Visdat_DataHIV.py

--- Visdat_DataHIV.py
+++ Visdat_DataHIV.py
@@ -3,9 +3,13 @@ import pandas as pd
 from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource, HoverTool
 from bokeh.layouts import column
+from io import BytesIO
 
 # Load the HIV dataset
 hiv_data = pd.read_csv('hiv.csv')
+
+# Set up the data source
+source = ColumnDataSource(hiv_data)
 
 # Create the figure
 p = figure(plot_width=400, plot_height=400, x_axis_label='Jumlah Kasus', y_axis_label='Kelompok Umur')
@@ -17,7 +21,7 @@ p.add_tools(hover)
 # Create the scatter plot glyphs for each gender
 gender_glyphs = {}
 for gender in hiv_data['jenis_kelamin'].unique():
-    gender_glyphs[gender] = p.circle('jumlah_kasus', 'kelompok_umur', source=None, size=8, color='red', alpha=0.5)
+    gender_glyphs[gender] = p.circle('jumlah_kasus', 'kelompok_umur', source=source, size=8, color='red', alpha=0.5)
 
 # Create the select widget
 gender_list = ['All'] + list(hiv_data['jenis_kelamin'].unique())
@@ -27,31 +31,22 @@ select_gender = st.selectbox('Jenis Kelamin', options=gender_list)
 slider = st.slider('Tahun', min_value=min(hiv_data['tahun']), max_value=max(hiv_data['tahun']), value=min(hiv_data['tahun']), step=1)
 
 # Update the plot based on the selected gender and year
-def update_plot():
-    selected_data = hiv_data[(hiv_data['jenis_kelamin'] == select_gender) | (select_gender == 'All') & (hiv_data['tahun'] == slider)]
-    source.data = ColumnDataSource(selected_data).data
-
-# Initialize the data source
-source = ColumnDataSource(data=dict(jumlah_kasus=[], kelompok_umur=[], nama_kabupaten_kota=[], jenis_kelamin=[], tahun=[]))
-
-# Update the plot initially
-update_plot()
+selected_data = hiv_data[(hiv_data['jenis_kelamin'] == select_gender) | (select_gender == 'All') & (hiv_data['tahun'] == slider)]
+source.data.update(ColumnDataSource(selected_data).data)
 
 # Update the glyph fill alpha based on the selected gender
-def update_glyphs():
-    for gender, glyph in gender_glyphs.items():
-        if select_gender == 'All' or gender == select_gender:
-            glyph.glyph.fill_alpha = 0.5
-        else:
-            glyph.glyph.fill_alpha = 0
+for gender, glyph in gender_glyphs.items():
+    if select_gender == 'All' or gender == select_gender:
+        glyph.glyph.fill_alpha = 0.5
+    else:
+        glyph.glyph.fill_alpha = 0
 
 # Create the layout
 layout = column(p)
 
-# Display the plot and layout
-st.bokeh_chart(layout)
+# Convert the plot to image
+plot_img = BytesIO()
+p.save(plot_img, format='png')
 
-# Run the update functions when the select box or slider values change
-if select_gender is not None and slider is not None:
-    update_plot()
-    update_glyphs()
+# Display the plot as an image
+st.image(plot_img)
